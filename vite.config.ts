c68a067be05d8106f3c40e391528f773d5ba6798
keibn29 <keibn29@gmail.com>
2024-03-12 14:29:21 +0700

    [UPDATE] types for package

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { resolve } from 'path';
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts'
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve("src", "src/main.tsx"),
+      entry: resolve(__dirname, "src/main.tsx"),
       name: "xlsx-merge-ts",
       fileName: (format) => `xlsx-merge-ts.${format}.js`,
     },
@@ -19,5 +20,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"] })],
 });
