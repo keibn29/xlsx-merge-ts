2feb599dad9d0cda104da5b0a82369ee5c325138
keibn29 <keibn29@gmail.com>
2024-03-13 12:00:19 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: ["es2020"],
     lib: {
       entry: resolve(__dirname, "src/main.tsx"),
       name: "xlsx-merge-ts",
@@ -29,6 +28,6 @@ export default defineConfig({
     }),
   ],
   optimizeDeps: {
-    exclude: ["three-gpu-pathtracer", "sqlite-wasm-esm"],
+    exclude: ["three-gpu-pathtracer"],
   },
 });
